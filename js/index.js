06f8aace847327b8dfe958d55cfc8f5cd340d34c
ervand96 <ervandharutyunyan06096@gmail.com>
2022-02-14 04:49:17 -0800

    fixed - comments

--- js/index.js
+++ js/index.js
@@ -19,17 +19,21 @@ const arrayColor = [{
 let degCount = 0;
 
 function changeColorButton() {
-    let glbSquare = document.querySelector('.glbSquare');
-
-    for (let i = 0; i < glbSquare.children.length; i++) {
-        glbSquare.children[i].style.backgroundColor = arrayColor[i].color;
+    const glbSquare = document.querySelector('.glbSquare');
+    const glbChild = glbSquare.children;
+    if (glbChild.length) {
+        for (let i = 0; i < glbSquare.children.length; i++) {
+            glbSquare.children[i].style.backgroundColor = arrayColor[i].color;
+        }
     }
-
 }
 
 function changeColor() {
-    let glbSquare = document.querySelector('.glbSquare');
+    const glbSquare = document.querySelector('.glbSquare');
     degCount += 90;
+    if (degCount >= 360) {
+        degCount = 0;
+    }
     glbSquare.style.transform = `rotate(${degCount}deg)`;
 }
 
